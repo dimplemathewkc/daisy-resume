52cf1f82bd45fb8a39ffe69ff596d37f2bca7fd8
dimple5 <dimple@vernacular.ai>
2021-09-12 23:31:34 +0530

    Updates

---
+++ precache-manifest.a0e2980a8c5bcc410cdb936b5c3de2d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30154fa9c13cbb783440badcb61f787d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e842f1b0fff07c738f97",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "7ff0b9474a66c8bcd955",
+    "url": "/static/css/main.ec6ed111.chunk.css"
+  },
+  {
+    "revision": "e842f1b0fff07c738f97",
+    "url": "/static/js/2.a4730dfc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.a4730dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff0b9474a66c8bcd955",
+    "url": "/static/js/main.ec0d9c9e.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
